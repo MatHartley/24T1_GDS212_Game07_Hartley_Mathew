1a65e37a6ec3a1bd2b532b78d56fd4bb6b7d8bc0
MatHartley_HomePC <115046381+MatHartley@users.noreply.github.com>
2024-03-26 13:32:22 +1100

    26/03/24
    Got overlap circle working (I think)

--- Assets/Scripts/AudioEmission.cs
+++ Assets/Scripts/AudioEmission.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class AudioEmission : MonoBehaviour
 {
+    [SerializeField] float radius = 2;
+    List<Collider2D> collisions = new List<Collider2D>();
 
     // Update is called once per frame
     void Update()
     {
-        
+        var contactFilter = new ContactFilter2D();
+        contactFilter.SetLayerMask(LayerMask.GetMask("Enemy"));
+        contactFilter.useLayerMask = true;
+        Physics2D.OverlapCircle(transform.position, radius, contactFilter, collisions);
+
+        foreach (Collider2D nearbyObject in collisions)
+        {
+            Debug.Log("Detected");
+        }
     }
 }
