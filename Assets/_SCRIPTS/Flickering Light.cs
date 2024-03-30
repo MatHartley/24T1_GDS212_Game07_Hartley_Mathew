f00851e47263c31a7dcbec142e41f13d03d8afa7
MatHartley_HomePC <115046381+MatHartley@users.noreply.github.com>
2024-03-30 11:39:35 +1100

    30/03/24
    Added flickering lights
    Enemy patrol routes have stopped working somehow? Also, player movement gets stuck just after first group of crates, removing patrolling enemies fixes player movement???

---
+++ Assets/_SCRIPTS/Flickering Light.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    [Header("Flicker Counters")]
+    [SerializeField] float offTime;
+    private float offCount;
+    [SerializeField] float onMax;
+    private float onCount;
+
+    [Header("Internals")]
+    private GameObject lightBeam;
+    private bool isOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightBeam = this.transform.GetChild(0).gameObject;
+        Debug.Log("Hello" + lightBeam.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isOn)
+        {
+            lightBeam.SetActive(false);
+            offCount += Time.deltaTime;
+            if (offCount >= offTime)
+            {
+                isOn = true;
+                onCount = 0;
+            }
+        }
+        else if (isOn)
+        {
+            lightBeam.SetActive(true);
+            onCount += Time.deltaTime;
+            if (onCount >= Random.Range(0, onMax))
+            {
+                isOn = false;
+                offCount = 0;
+            }
+        }
+    }
+}
